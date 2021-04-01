177125ad30c2fcf6793948b86dfd1f68996e57b8
Jennifer Toops <jennifertoops@gmail.com>
2021-04-01 10:11:37 -0400

    added public folder, updated all files so API runs locally

--- public/js/main.js
+++ public/js/main.js
@@ -2,11 +2,13 @@ alert("hi")
 document.querySelector('button').addEventListener('click', getRapName)
 
 async function getRapName() {
-    try{
-    const res = await fetch('http://localhost:8000/api/savage')
+    const rapName = document.querySelector('input').value
+    try {
+    const res = await fetch(`http://localhost:8000/api/rappers/${rapName}`)
     const data = await res.json()
     console.log(data)
+    document.querySelector('h2').innerText = data.birthName
     } catch(err){
     console.log(data)
-}
+    }
 }
--- server.js
+++ server.js
@@ -10,18 +10,42 @@ app.use(cors())
 app.use(express.static('public'))
 app.use(express.static('files'))
 
-let savage = {
-    'age': 28,
-    'birthName': 'Sheyaa Bin Abraham=Joseph',
-    'birthLocation': 'London, England'
+
+let rappers = {
+    '21 savage': {
+        'age': 28,
+        'birthName': 'Sheyaa Bin Abraham Joseph',
+        'birthLocation': 'London, England'
+    },
+    'chance the rapper': {
+        'age': 27,
+        'birthName': 'Chancelor Jonathan Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    },
+    'dylan': {
+        'age': 'dylan',
+        'birthName': 'dylan',
+        'birthLocation': 'dylan'
+    },
+    'unknown': {
+        'age': 'unknown',
+        'birthName': 'unknown',
+        'birthLocation': 'unknown',
+    }
 }
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api/savage', (request, response) => {
-    response.json(savage)
+app.get('/api/rappers/:rapperName', (request, response) => {
+    const rapName = request.params.rapperName.toLowerCase()
+    console.log(rapName)
+    if(rappers[rapName]){
+        response.json(rappers[rapName])
+    } else {
+        response.json(rappers['unknown'])
+    }
 })
 
 app.listen(PORT, () => {
